010cab3d23cf0c7857c0d8f5afd58d766cd7240a
oneore <joanorellanarios@gmail.com>
2018-05-18 11:19:37 +0200

    canvi de nom

--- escriu0.py
+++ escriu0.py
@@ -1,6 +1,6 @@
 from direct_red0 import *
 
-f=open("txt/no_tears_left_to_cry.txt", 'w')
+f=open("txt/No_Tears_Left_To_Cry_1_-_Ariana_Grande.txt", 'w')
 for i in freqs_t:
     f.write("%s" % i[1])
     if i!=freqs_t[-1]:
