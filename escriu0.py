c63dd93cee28332eab833530856136a7be125140
oneore <joanorellanarios@gmail.com>
2018-05-30 10:26:46 +0200

    reconeix sum copy

--- escriu0.py
+++ escriu0.py
@@ -1,9 +1,8 @@
 from direct_red0 import *
 
-f=open("txt/Nocturne_-_Frederic_Chopin.txt", 'w')
+f=open("txt/Shape_Of_You_-_Ed_Sheeran.txt", 'w')
 for i in freqs_t:
     f.write("%s" % i[1])
     if i!=freqs_t[-1]:
         f.write("\n")
-
 f.close()
--- reconeix_sum2.py
+++ reconeix_sum2.py
@@ -2,13 +2,9 @@ import pyaudio
 import numpy as np
 import wave
 import glob, os
-from lcd import *
 
 p = pyaudio.PyAudio()
 os.chdir("txt")
-
-clear_screen()
-missatge("Escoltant...")
 
 CHUNK = 8192
 FORMAT = pyaudio.paInt16
@@ -52,9 +48,6 @@ stream.stop_stream()
 stream.close()
 p.terminate()
 
-clear_screen()
-missatge("Analitzant...")
-
 guess = ''
 guess_song = ''
 
@@ -97,11 +90,8 @@ for i in range(len(guess)):
         guess_song1=guess_song[0:15]
         guess_song2=guess_song[15:]
         guess_song3=guess_song1 + "\n" + guess_song2
-clear_screen()
 if sumes[1][0]/sumes[0][0]<0.8 and sumes[0][0]-sumes[1][0]>5:
     print(sumMin)
     print(guess_song)
-    missatge(guess_song3)
 else:
     print("No s'ha pogut trobar")
-    missatge(" No s'ha pogut\n     trobar")
