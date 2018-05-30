042f617dbb9043025ac8ceb4897a37c1c815c80a
oneore <joanorellanarios@gmail.com>
2018-05-30 18:54:51 +0200

    clear

--- button.py
+++ button.py
@@ -5,6 +5,7 @@ import os
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
     input_state = GPIO.input(4)
