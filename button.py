fde797ad9c3249b72540aaa8aaed74d8dcf4efde
oneore <joanorellanarios@gmail.com>
2018-05-30 19:00:24 +0200

    buton

--- button.py
+++ button.py
@@ -6,12 +6,8 @@ from lcd import *
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
     input_state = GPIO.input(4)
-    input_state2 = GPIO.input(24)
     if input_state == False:
         os.system('python reconeix_sum_copy.py')
-    if input_state2 == False:
-        clear_screen()
