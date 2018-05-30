afeed37323ef9957d35834cccd85c5a9d8624b71
oneore <joanorellanarios@gmail.com>
2018-05-30 10:37:11 +0200

    else

--- button.py
+++ button.py
@@ -9,4 +9,4 @@ GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 while True:
     input_state = GPIO.input(4)
     if input_state == False:
-        os.system('python reconeix_sum.py')
+        os.system('python reconeix_sum_copy.py')
