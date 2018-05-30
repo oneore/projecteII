6f8bae235ab107174a35fb59d93d98de39dd7300
oneore <joanorellanarios@gmail.com>
2018-05-30 10:44:09 +0200

    clair de lune

--- escriu0.py
+++ escriu0.py
@@ -1,6 +1,6 @@
 from direct_red0 import *
 
-f=open("txt/Le_Moulin_-_Yann_Tiersen_(Amelie).txt", 'w')
+f=open("txt/Clair_de_lune_-_Claude_Debussy.txt", 'w')
 for i in freqs_t:
     f.write("%s" % i[1])
     if i!=freqs_t[-1]:
