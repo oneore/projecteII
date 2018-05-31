de80d51e659e3cd92085c3415d2b654c909d73ab
oneore <joanorellanarios@gmail.com>
2018-05-31 16:33:25 +0200

    one kiss

--- escriu0.py
+++ escriu0.py
@@ -1,6 +1,6 @@
 from direct_red0 import *
 
-f=open("txt/Clair_De_Lune_-_Claude_Debussy.txt", 'w')
+f=open("txt/One_Kiss_-_Dua_Lipa.txt", 'w')
 for i in freqs_t:
     f.write("%s" % i[1])
     if i!=freqs_t[-1]:
