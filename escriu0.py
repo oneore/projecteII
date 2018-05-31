0d1c83bfb042cac3e5ba3e56f502ddec963546d1
oneore <joanorellanarios@gmail.com>
2018-05-31 17:01:07 +0200

    red

--- escriu0.py
+++ escriu0.py
@@ -1,6 +1,6 @@
 from direct_red0 import *
 
-f=open("txt/Back_To_Black_-_Amy_Winehouse.txt", 'w')
+f=open("txt/Red_Red_Wine_-_UB40.txt", 'w')
 for i in freqs_t:
     f.write("%s" % i[1])
     if i!=freqs_t[-1]:
