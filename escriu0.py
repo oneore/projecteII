45d13ad8a101d50a207d12ebf02baf0dd2b48215
oneore <joanorellanarios@gmail.com>
2018-06-01 11:03:50 +0200

    nocturne

--- escriu0.py
+++ escriu0.py
@@ -1,6 +1,6 @@
 from direct_red0 import *
 
-f=open("txt/Safe_&_Sound_-_Capital_Cities.txt", 'w')
+f=open("txt/Nocturne_-_Frederic_Chopin.txt", 'w')
 for i in freqs_t:
     f.write("%s" % i[1])
     if i!=freqs_t[-1]:
